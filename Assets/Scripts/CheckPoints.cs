f898484c7102fd758a8f9a71f83ce4ae435b8746
MardookStone <BackTrack1984@hotmail.com>
2024-01-28 11:05:38 +0000

    Player collision checking
    Camera and checkpoint updates too

---
+++ Assets/Scripts/CheckPoints.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoints : MonoBehaviour
+{
+    public static CheckPoints Instance;
+    //make this a singleton so players can get instance of it?
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    public Transform[] checkPoints;
+    public Transform[] cameraLevelPositions;
+    public int checkPointIndex; //where the respawn point is!
+    public int cameraLevelPositionIndex;
+
+    public void UpdateCheckPoint()
+    {
+        checkPointIndex++;
+
+    }
+
+    public Vector2 GetCurrentCheckPoint()
+    {
+        return checkPoints[checkPointIndex].position;
+    }
+
+    public void ChangeCameraPosition()
+    {
+        cameraLevelPositionIndex++;
+        Camera.main.transform.position = cameraLevelPositions[cameraLevelPositionIndex].position;
+    }
+
+}
